3245bf26a7e6f4167f5ba372e25039ea1fd4ae43
vmyakotin <vmyakotin@ptsecurity.com>
2024-03-06 03:14:58 +0300

    Добавьте файлы проекта.

---
+++ Standoff Service/Database.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standoff_Service
+{
+    class Database
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=localtest");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ Standoff Service/LoginWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Standoff_Service
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UsernameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            textBox.Text = textBox.Text.Replace("Логин:", string.Empty);
+            textBox.Foreground = new SolidColorBrush(Colors.Black);
+
+        }
+
+        private void UsernameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.Foreground = new SolidColorBrush(Colors.Gray);
+                textBox.Text = "Логин:";
+            }
+        }
+
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordTextBox = (PasswordBox)sender;
+
+            if (passwordTextBox.Password == "Пароль:")
+            {
+                passwordTextBox.Password = string.Empty;
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordTextBox = (PasswordBox)sender;
+
+            if (string.IsNullOrEmpty(passwordTextBox.Password))
+            {
+                passwordTextBox.Password = "Пароль:";
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string loginUser = LoginField.Text;
+            string passwordUser = PasswordField.Password;
+
+            Database database = new Database();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand($"SELECT * FROM users WHERE login = '{loginUser}' AND password = '{passwordUser}'", database.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                ErrorMessageTextBlock.Visibility = Visibility.Visible;
+                ErrorMessageTextBlock.Text = "Ебал я это восстанавливать";
+            }
+            else
+            {
+                ErrorMessageTextBlock.Visibility = Visibility.Visible;
+                ErrorMessageTextBlock.Text = "Ошибка йопта";
+            }
+        }
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+        }
+    }
+}
